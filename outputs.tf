ec1aa023687cd4fbe0beec804c9319ac04602233
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2020-12-28 11:16:27 +0900

    Remember to remove now dead values from outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,16 +3,6 @@ output "runner_as_group_name" {
   value       = aws_autoscaling_group.gitlab_runner_instance.name
 }
 
-output "runner_cache_bucket_arn" {
-  description = "ARN of the S3 for the build cache."
-  value       = module.cache.arn
-}
-
-output "runner_cache_bucket_name" {
-  description = "Name of the S3 for the build cache."
-  value       = module.cache.bucket
-}
-
 output "runner_agent_role_arn" {
   description = "ARN of the role used for the ec2 instance for the GitLab runner agent."
   value       = aws_iam_role.instance.arn
@@ -21,16 +11,6 @@ output "runner_agent_role_arn" {
 output "runner_agent_role_name" {
   description = "Name of the role used for the ec2 instance for the GitLab runner agent."
   value       = aws_iam_role.instance.name
-}
-
-output "runner_role_arn" {
-  description = "ARN of the role used for the docker machine runners."
-  value       = aws_iam_role.docker_machine.arn
-}
-
-output "runner_role_name" {
-  description = "Name of the role used for the docker machine runners."
-  value       = aws_iam_role.docker_machine.name
 }
 
 output "runner_agent_sg_id" {
